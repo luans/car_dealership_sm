8b6d4830ac5f9ba9082fa2022ffa76f57bcc0530
Rafael Belo <rafaelrpbelo@gmail.com>
2015-12-26 15:12:35 -0300

    Refactor customer specs

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -26,45 +26,31 @@ RSpec.describe Customer do
     )
   end
 
+  it 'should can create a new user' do
+    expect{ subject.save }.to change { subject.persisted? }.to true
+  end
+
   context 'validations' do
-    it { should be_valid }
+    it { is_expected.to be_valid }
+    it { is_expected.to validate_presence_of(:full_name) }
+    it { is_expected.to validate_presence_of(:cpf) }
+    it { is_expected.to validate_length_of(:place_birth_uf).is_equal_to(2) }
 
-    it 'should can create a new user' do
-      expect{ subject.save }.to change { subject.persisted? }.to true
-    end
-
-    context 'presence' do
-      it '.full_name should be present' do
-        expect{ subject.full_name = nil }.to change { subject.valid? }.to false
-        expect(subject.errors.full_messages).to include 'Nome completo não pode ficar em branco'
-      end
-
-      it '.cpf should be present' do
-        expect{ subject.cpf = nil }.to change { subject.valid? }.to false
-        expect(subject.errors.full_messages).to include 'CPF não pode ficar em branco'
-      end
+    it 'should require email has a valid format' do
+      subject.email = 'jhon.doe@example.com'
+      expect(subject).to be_valid
     end
 
     context 'date' do
-      it '.birth_date old should be enough (18 or more)' do
+      it 'should require old should to be enough (18 or more)' do
         expect{ subject.birth_date = Date.today - 17.years }.to change { subject.valid? }.to false
         expect(subject.errors.full_messages).to include 'Data de nascimento não é válido'
       end
 
-      it '.rg_expedition should be equal or less the current date' do
+      it 'should require rg_expedition should be equal or less the current date' do
         expect{ subject.rg_expedition = Date.today + 1 }.to change { subject.valid? }.to false
         expect(subject.errors.full_messages).to include 'Data de expedição não é válido'
       end
-    end
-
-    it '.place_birth_uf should be 2 characteres' do
-      expect{ subject.place_birth_uf = 'P' }.to change { subject.valid? }.to false
-      expect(subject.errors.full_messages).to include 'UF não possui o tamanho esperado (2 caracteres)'
-    end
-
-    it '.email should be format valid' do
-      subject.email = 'jhon.doe@example.com'
-      expect(subject).to be_valid
     end
   end
 end
