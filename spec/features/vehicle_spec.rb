72b9a26992a4f16b7971109847f0da49b1b5dd22
Rafael Belo <rafaelrpbelo@gmail.com>
2015-12-27 01:19:47 -0300

    Refactor feature test of vehicle

--- spec/features/vehicle_spec.rb
+++ spec/features/vehicle_spec.rb
@@ -4,22 +4,22 @@ describe Vehicle do
   it 'create' do
     visit new_vehicle_path
 
-    fill_in 'Brand', with: 'Audi'
-    fill_in 'Version', with: 'A4 2.0 AT'
-    fill_in 'Maker year', with: 2014
-    fill_in 'Model year', with: 2014
-    fill_in 'License plate', with: "AAA0000"
-    fill_in 'City', with: 'Teresina'
-    fill_in 'Uf', with: 'PI'
-    fill_in 'Color', with: 'Vermelho'
-    fill_in 'Fuel', with: 'Flex'
+    fill_in 'Marca', with: 'Audi'
+    fill_in 'Versão', with: 'A4 2.0 AT'
+    fill_in 'Ano fabricação', with: 2014
+    fill_in 'Ano modelo', with: 2014
+    fill_in 'Placa', with: "AAA0000"
+    fill_in 'Cidade', with: 'Teresina'
+    fill_in 'UF', with: 'PI'
+    fill_in 'Cor', with: 'Vermelho'
+    fill_in 'Combustível', with: 'Flex'
     fill_in 'Renavam', with: '10203040'
     fill_in 'Chassi', with: '9BWZZZ377VT004251'
-    fill_in 'Owner', with: 'Jhon Doe'
-    fill_in 'Old license plate', with: 'BBB-0000'
+    fill_in 'Proprietário', with: 'Jhon Doe'
+    fill_in 'Placa anterior', with: 'BBB-0000'
     select 'Estoque', from: 'Status'
 
-    click_on 'Criar Vehicle'
+    click_on 'Criar Veículo'
 
     expect(page).to have_content 'Audi'
     expect(page).to have_content 'A4 2.0 AT'
@@ -36,8 +36,7 @@ describe Vehicle do
     expect(page).to have_content 'BBB-0000'
     expect(page).to have_content 'Estoque'
 
-
-    expect(page).to have_content 'Vehicle created successful'
+    expect(page).to have_content 'Veículo criado com sucesso'
   end
 
   it 'edit' do
@@ -60,22 +59,22 @@ describe Vehicle do
 
     visit edit_vehicle_path(vehicle)
 
-    fill_in 'Brand', with: 'Audi edited'
-    fill_in 'Version', with: 'TT 3.0 Aut'
-    fill_in 'Maker year', with: 2015
-    fill_in 'Model year', with: 2015
-    fill_in 'License plate', with: 'AAA1111'
-    fill_in 'City', with: 'Timon'
-    fill_in 'Uf', with: 'MA'
-    fill_in 'Color', with: 'Branco'
-    fill_in 'Fuel', with: 'Gasolina'
+    fill_in 'Marca', with: 'Audi edited'
+    fill_in 'Versão', with: 'TT 3.0 Aut'
+    fill_in 'Ano fabricação', with: 2015
+    fill_in 'Ano modelo', with: 2015
+    fill_in 'Placa', with: 'AAA1111'
+    fill_in 'Cidade', with: 'Timon'
+    fill_in 'UF', with: 'MA'
+    fill_in 'Cor', with: 'Branco'
+    fill_in 'Combustível', with: 'Gasolina'
     fill_in 'Renavam', with: '10203041'
     fill_in 'Chassi', with: '9BWZZZ377VT004252'
-    fill_in 'Owner', with: 'Jhon Cena'
-    fill_in 'Old license plate', with: 'BBB-1111'
+    fill_in 'Proprietário', with: 'Jhon Cena'
+    fill_in 'Placa anterior', with: 'BBB-1111'
     select 'Vendido', from: 'Status'
 
-    click_on 'Atualizar Vehicle'
+    click_on 'Atualizar Veículo'
 
     expect(page).to have_content 'Audi edited'
     expect(page).to have_content 'TT 3.0 Aut'
